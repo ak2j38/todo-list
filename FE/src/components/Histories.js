44076bb3b5188da9bec4d5925a2d7615c68036f6
Benny <77577858+uBeom@users.noreply.github.com>
2022-04-14 19:16:36 +0900

    Revert "[Benny] 활동기록 API 요청 및 파싱"

--- FE/src/components/Histories.js
+++ FE/src/components/Histories.js
@@ -1,14 +1,18 @@
 import HistoryCard from "./HistoryCard.js";
 import { store } from "../store.js";
+import { GET_HISTORY } from "../dummyData.js";
 import { $ } from "../utils.js";
 
 export default class Histories {
   constructor(target) {
     this.target = target;
+    this.historyCardComponents = GET_HISTORY.map(
+      (history) => new HistoryCard(history)
+    );
     store.initState("history");
     store.subscribe("history", this.render.bind(this));
+    store.setState("history", GET_HISTORY);
   }
-
   template() {
     return `
         <button class="history__btn--close">x</button>
@@ -18,28 +22,12 @@ export default class Histories {
         )}
     `;
   }
-
   render() {
     this.target.innerHTML = this.template();
   }
-
   addEvent() {
-    $(".header__menu").addEventListener("click", () => {
-      showHistoryView();
-<<<<<<< HEAD
-      const historyData = requestHistoryData();
-      this.historyCardComponents = setHistoryDataToHistoryCard(historyData);
-=======
-      const historyData = requestData();
-      this.historyCardComponents = historyData.map(
-        (history) => new HistoryCard(history)
-      );
->>>>>>> 930f577e93865b62fc46aba33cbf68286b273141
-      store.setState("history", historyData);
-    });
-    $(".history__btn--close").addEventListener("click", () => {
-      hideHistoryView();
-    });
+    $(".header__menu").addEventListener("click", showHistoryView);
+    $(".history__btn--close").addEventListener("click", hideHistoryView);
   }
 }
 
@@ -49,23 +37,4 @@ function showHistoryView() {
 
 function hideHistoryView() {
   $(".history").style.right = "-45.2rem";
-}
-
-<<<<<<< HEAD
-function setHistoryDataToHistoryCard(historydata) {
-  return historydata.map((history) => new HistoryCard(history));
-}
-
-async function requestHistoryData() {
-=======
-async function requestData() {
->>>>>>> 930f577e93865b62fc46aba33cbf68286b273141
-  const url = "http://3.38.224.138/histories";
-  const res = await fetch(url, {
-    mode: "cors",
-    credentials: "include",
-  });
-  const data = res.json();
-
-  return data;
 }
